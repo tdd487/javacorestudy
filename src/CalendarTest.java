fa6de72bead30cd36b2644cf29b9c5098de3404b
tdd487 <168700784@qq.com>
2016-08-05 18:08:49 +0800

    carlendarTest

--- src/CalendarTest.java
+++ src/CalendarTest.java
@@ -1,5 +1,15 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * Created by tdd on 2016/8/5.
  */
 public class CalendarTest {
+    public static void main(String args[]){
+        GregorianCalendar d = new GregorianCalendar();
+        int today = d.get(Calendar.DAY_OF_MONTH);
+        int month = d.get(Calendar.MONTH);
+        System.out.println(today+"----"+month);
+
+    }
 }
